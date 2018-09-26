01097b8dd304a23b56b6e7956fa1a5f63c531d71
Matthew <matthew@linux-pc>
2018-09-26 20:05:18 +1000

    Forgot boot signature

--- examples/asm/hide_partition.asm
+++ examples/asm/hide_partition.asm
@@ -9,4 +9,17 @@
 
 _start:
     cli
-    
+    xor ax, ax
+
+    mov ds, ax  ; Set data segment to 0
+    mov es, ax  ; extra segment = 0
+    mov ss, ax  ; stack segment = 0
+    xor sp, sp  ; stack pointer = 0
+
+    mov di, 0x0600   ; new memory addr for this MBR
+    mov si, 0x7c00  ; where this MBR is currently at
+    mov cx, 0x0100  ; size of the MBR (0x1000 = 512)
+
+    rep movsw
+
+    xor ax, ax

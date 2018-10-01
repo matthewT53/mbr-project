59fb6c62a45b00e6d726e0a5d6d87b89413d1956
Matthew <matthew@linux-pc>
2018-10-01 19:11:55 +1000

    Able to print to screen and write to sector now

--- examples/asm/hide_partition.asm
+++ examples/asm/hide_partition.asm
@@ -9,7 +9,7 @@
 [org 0x1000]
 
 %define SECTOR_READ_ADDR    0x1500
-%define STACK_ADDR          0x0500
+%define STACK_ADDR          0x600
 
 _start:
     cli
@@ -26,29 +26,80 @@ _start:
     mov cx, 0x0100              ; size of the MBR (0x100 = 256) but we are writing words
 
     rep movsw                   ; copy the mbr to 0x1000
-    jmp 0:_reset_disk           ; jumps to new address starting at 0x600
+    jmp 0:_read_disk            ; jumps to new address starting at 0x600
+
+_read_disk:
+    call _reset_disk
+
+    mov si, reading_disk_msg
+    call _print_str
+
+    mov al, 0x1                 ; num of sectors to read
+    mov ch, 0x0                 ; cylinder to read from
+    mov dh, 0x0                 ; head to read from
+    mov cl, 0x2                 ; sector to read from
+    mov bx, SECTOR_READ_ADDR    ; where to store the sector we read
+    call _read_sector
+
+    mov cl, 0x4
+    call _write_sector
+
+    mov cl, 0x5
+    call _write_sector
+
+    mov cl, 0x6
+    call _write_sector
+    
+    hlt
 
 _reset_disk:
     mov ah, 0x0
     int 0x13                    ; reset the disk
-
-    call _read_sector
-    hlt
-
-_read_sector:
-    push bx
-    mov al, 0x1                 ; num of sectors to read
-    mov ch, 0x0                 ; cylinder to read from
-    mov dh, 0x00                ; head to read from
-    mov cl, 0x2                 ; sector to read from
-    mov bx, SECTOR_READ_ADDR    ; where to store the sector we read
-    mov ah, 0x2                 ; code for specifying reading sectors
-    int 0x13
-    pop bx
+    jc _reset_error
     ret
 
-mesg_1: db "Wiping partition 1.", 0x00
-mesg_2: db "Error reading sector.", 0x00
+_read_sector:
+    mov ah, 0x2                 ; code for specifying reading sectors
+    int 0x13
+    jc _read_error
+    ret
+
+_write_sector:
+    mov ah, 0x3
+    int 0x13
+    jc _write_error
+    ret
+
+_write_error:
+    mov si, write_error_msg
+    call _print_str
+    hlt
+
+_reset_error:
+    mov si, reset_disk_error_msg
+    call _print_str
+    hlt
+
+_read_error:
+    mov si, read_disk_error_msg
+    call _print_str
+    hlt
+
+_print_str:
+    lodsb
+    or al, al
+    jz _print_end
+    mov ah, 0xe
+    int 0x10
+    jmp _print_str
+
+_print_end:
+    ret
+
+reading_disk_msg:           db "Reading sector 1 of disk!", 0x00
+reset_disk_error_msg:       db "Error resetting disk.", 0x00
+read_disk_error_msg:        db "Error reading sector.", 0x00
+write_error_msg:            db "Error writing sector.", 0x00
 
 ; fill the mbr will null bytes to acquire the 512 byte size
 times 0x1be - ($ - $$) db 0x00

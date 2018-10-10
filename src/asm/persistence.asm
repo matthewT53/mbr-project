79d91d7209a56d6a5f4dd79a2ddfac9359bd575d
Matthew <matthew@linux-pc>
2018-10-10 16:47:21 +1100

    Finished coding persistence

--- src/asm/persistence.asm
+++ src/asm/persistence.asm
@@ -27,8 +27,14 @@
 %define SECTORS_PER_FAT_OFFSET          0x24    ; 4 bytes
 %define ROOT_CLUSTER_OFFSET             0x2c    ; 4 bytes
 
+; offsets into the root directory
+%define NEW_ENTRY_OFFSET 0x40
+
 _start:
     pusha
+
+    mov BYTE [drive_type], dl
+
     mov bx, NEW_MBR_ADDR + MBR_PART_OFFSET
     mov cx, 0x4
 
@@ -93,10 +99,11 @@ _process_vbr:
 
     ; add 2 * sectors_per_fat
     mov ecx, DWORD [sectors_per_fat]
-    mov eax, 2                          ; assume there are 2 FAT tables
+    mov eax, 2                          ; assume there are only 2 FAT tables
     mul ecx                             ; result is stored in edx:aax
 
     add edi, eax
+    mov DWORD [root_dir_lba_addr], edi
 
     ; read in the root directory
     sub sp, 0x10
@@ -112,12 +119,72 @@ _process_vbr:
 
     call _read_sector_lba
     add sp, 0x10
-    
-    ; add en entry into the root directory
 
-    ; copy back the modified root directory
+    ; add en entry into the root directory
+    lea esi, [root_dir_entry]
+    lea edi, [ROOT_DIR_ADDR + NEW_ENTRY_OFFSET]
+    mov cx, 0x20
+
+    rep movsb
+
+    ; copy back the modified root directory onto disk
+    sub sp, 0x10
+    mov dl, BYTE [drive_type]
+    mov edi, DWORD [root_dir_lba_addr]
+
+    push DWORD 0x0
+    push edi
+
+    push 0x0
+    push ROOT_DIR_ADDR
+
+    push 0x1
+    push 0x10
+
+    mov si, sp
+    call _write_sector_lba
+    add sp, 0x10
 
     ; copy data to the first cluster of the directory entry
+    ; load sector 3 into RAM
+    sub sp, 0x10
+    mov dl, BYTE [drive_type]
+    mov edi, DWORD [root_dir_lba_addr]
+
+    push DWORD 0x0
+    push DWORD 0x2
+
+    push 0x0
+    push SECTOR_THREE_ADDR
+
+    push 0x1
+    push 0x10
+
+    mov si, sp
+    call _read_sector_lba
+    add sp, 0x10
+
+    ; calculate LBA of cluster 3
+    mov esi, edi
+    mov ecx, [sectors_per_cluster]
+    add esi, ecx
+
+    ; write sector 3 data to cluster 3
+    sub sp, 0x10
+    mov dl, BYTE [drive_type]
+
+    push DWORD 0x0
+    push esi
+
+    push 0x0
+    push SECTOR_THREE_ADDR
+
+    push 0x1
+    push 0x10
+
+    mov si, sp
+    call _write_sector_lba
+    add sp, 0x10
 
 _end:
     popa
@@ -129,14 +196,14 @@ _reset_disk:
     jc _disk_io_error
     ret
 
-_read_sector_chs:
-    mov ah, 0x2
+_read_sector_lba:
+    mov ah, 0x42
     int 0x13
     jc _disk_io_error
     ret
 
-_read_sector_lba:
-    mov ah, 0x42
+_write_sector_lba:
+    mov ah, 0x43
     int 0x13
     jc _disk_io_error
     ret
@@ -175,8 +242,16 @@ not_a_vbr:                  db "[Persistence]: Not a fat vbr!", 0x0d, 0x0a, 0x00
 processing_fat_vbr_msg:     db "Processing FAT32 VBR!", 0x0d, 0x0a, 0x00
 disk_io_error_msg:          db "Disk IO error!", 0x0d, 0x0a, 0x00
 
+drive_type:              db 0x00
 sectors_per_cluster:     db 0x00
 start_of_fs_addr:        dd 0x00
 sectors_per_fat:         dd 0x00
 num_reserved_sectors:    dw 0x00
+
 root_dir_cluster_num:    dd 0x00
+root_dir_lba_addr:       dd 0x00
+
+root_dir_entry:          db 0x4D, 0x42, 0x52, 0x5F, 0x57, 0x49, 0x4E, 0x53, 0x54,
+                         db 0x58, 0x54, 0x20, 0x18, 0xC3, 0xF1, 0x81, 0x4A, 0x4D,
+                         db 0x4A, 0x4D ,0x00, 0x00, 0x18, 0x82, 0x4A, 0x4D, 0x03,
+                         db 0x00, 0x24, 0x00, 0x00, 0x00

